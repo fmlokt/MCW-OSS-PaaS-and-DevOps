32ec31697300c8d2b18f1152459886d7fc1727b1
Ubuntu <demouser@LabVM.rnlkrwcbolau3dek0gcm0zshyb.bx.internal.cloudapp.net>
2019-09-26 13:38:38 +0000

    add CI/move to az

--- Hands-on lab/lab-files/app.js
+++ Hands-on lab/lab-files/app.js
@@ -13,7 +13,7 @@ var userSession = require('./routes/session');
 
 var app = express();
 
-var databaseUrl = 'mongodb://localhost:27017/best-for-you-organics';
+var databaseUrl = 'mongodb://best-for-you-db-mbon2:zj0ZphRxUZGwmZFciPPNGoti1XMQrcHT73bOCEfQHgeGnkm8XgUZnUbX51obbfPlBkbUEOygok9gv1zwwakM6Q==@best-for-you-db-mbon2.documents.azure.com:10255/best-for-you-organics?ssl=true&replicaSet=globaldb'; //'mongodb://localhost:27017/best-for-you-organics';
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
